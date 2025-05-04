8e674ecff6ba1377895baf0f34159079eddd5558
Michael Cabrera <Michael.cabrera.0515@gmail.com>
2025-05-04 14:48:41 -0500

    Added Mobile Menu

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import '../styles/components/about.css';
 import '../styles/components/featured.css';
 import '../styles/components/work.css';
 import '../styles/components/contact.css';
+import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
 import '../styles/utils.css';
 
+import mobileNav from './utils/mobile-nav';
+
+mobileNav();
 
 // npm run dev - open site in browser
